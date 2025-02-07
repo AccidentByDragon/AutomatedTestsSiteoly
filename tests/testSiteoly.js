295f0126069c443734de0a4b9a5f97888d2f9680
AccidentByDragon <karl17loe@hotmail.com>
2025-02-07 14:00:25 +0100

    firstAttempt
    need to work on Element Selector

--- tests/testSiteoly.js
+++ tests/testSiteoly.js
@@ -1,7 +1,15 @@
-import { fixture } from "testcafe";
+import { fixture, Selector, test } from "testcafe";
+
 
 fixture`A set of tests for our Siteoly site`
   .page`https://id24test5.siteoly.com/`;
 
+const elementSelector = Selector('#input-class mt-12') // this isn't working
+const emailForm = elementSelector.find('email')
+const submitForm = elementSelector.find('submit')
+
 //Tests
-  
+test(`select an element`, async t => {
+    await t.typeText(emailForm, 'karl.l.hmg@gmail.com');
+    await t.click(submitForm);
+  })
