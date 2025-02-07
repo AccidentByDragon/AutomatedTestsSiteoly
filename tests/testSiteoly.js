b64eb2a1b9d42ce3bbe8c03138a1136232e658f2
AccidentByDragon <karl17loe@hotmail.com>
2025-02-07 15:44:01 +0100

    Sort of Functioning

--- tests/testSiteoly.js
+++ tests/testSiteoly.js
@@ -4,12 +4,11 @@ import { fixture, Selector, test } from "testcafe";
 fixture`A set of tests for our Siteoly site`
   .page`https://id24test5.siteoly.com/`;
 
-const elementSelector = Selector('#subscribe-form') //# for searching via ID
-const emailForm = elementSelector.find('#email');
+const subscribeSelector = Selector('#subscribe-form') //# for searching via ID
+const emailForm = subscribeSelector.find('#email');
 const submitForm = emailForm.find('#submit');
 
-//Tests
-test(`select an element`, async t => {
+test(`select and fill in Email`, async t => {
     await t.typeText(emailForm, 'karl.l.hmg@gmail.com');
-    await t.click(emailForm); 
+    await t.click(emailForm); //We want to click the sumbit button here but can't find the right element/ID
   })
